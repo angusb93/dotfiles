c6516efbcb6e558c0b2fab8c8619554117b153e9
Angus Buick <angus@msquared.io>
2025-11-26 14:15:51 +0000

    chore: configure tflint

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -16,6 +16,42 @@ return {
         nil_ls = {
           mason = false, -- Don't use Mason for nil_ls
         },
+        terraformls = {
+          mason = false, -- Don't use Mason for terraform-ls
+          -- Disable LSP formatting in favor of conform.nvim
+          on_attach = function(client, bufnr)
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+          end,
+        },
+      },
+    },
+  },
+
+  -- Configure linting with nvim-lint
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        terraform = { "tflint" },
+        tf = { "tflint" },
+      },
+      linters = {
+        tflint = {
+          cmd = "tflint", -- Uses system tflint from Nix
+        },
+      },
+    },
+  },
+
+  -- Configure formatting with conform.nvim
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        terraform = { "terraform_fmt" },
+        tf = { "terraform_fmt" },
+        ["terraform-vars"] = { "terraform_fmt" },
       },
     },
   },
